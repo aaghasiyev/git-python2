1c505739b8646e7daabb10ae14d09e979d0ee22c
aaghasiyev <azar.aghasiyev@gmail.com>
2021-09-13 00:46:51 -0700

    Added new script file

---
+++ myscript.py
@@ -0,0 +1,12 @@
+# Second homework
+a = 3
+b = 10
+c = 20
+avg = ((a + b + c) / 3)
+print("Average value of numbers =", int(avg))
+
+a = 1
+b = 2
+c = 5
+avg = ((a + b + c) / 3)
+print("Average value of numbers =", round(avg, 2))
