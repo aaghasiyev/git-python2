c7a728061c3fe369f32a7b0ea296cf5f320a1ec9
aaghasiyev <azar.aghasiyev@gmail.com>
2021-09-13 01:11:20 -0700

    Added new script to file

--- myscript.py
+++ myscript.py
@@ -1,4 +1,4 @@
-# Second homework
+# Number average
 a = 3
 b = 10
 c = 20
@@ -10,3 +10,14 @@ b = 2
 c = 5
 avg = ((a + b + c) / 3)
 print("Average value of numbers =", round(avg, 2))
+
+# Boolean value
+a=5
+b=15
+print(a>b)
+
+#Power
+x = 5
+y = 3
+power = x ** y
+print(power)
